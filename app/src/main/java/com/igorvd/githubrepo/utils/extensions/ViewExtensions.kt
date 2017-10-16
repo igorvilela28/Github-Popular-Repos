331ec64194ed5bcab0d65c1c855ae139b3f41f6c
Igor Vilela <igorvilela28@hotmail.com>
2017-10-16 19:45:30 -0200

    Add extensions and documentation for it

--- app/src/main/java/com/igorvd/githubrepo/utils/extensions/ViewExtensions.kt
+++ app/src/main/java/com/igorvd/githubrepo/utils/extensions/ViewExtensions.kt
@@ -7,22 +7,29 @@ import com.squareup.picasso.Picasso
 import com.squareup.picasso.RequestCreator
 
 /**
- * Contain extension for [View] objects
+ * Contains extension for the [View] class and subclasses
  * @author Igor Vilela
  * @since 16/10/17
  */
 
 
-
+/**
+ * Loads a image from URL using the [Picasso] library. It transforms the image into a circle,
+ * using the [CircleTransform] class
+ * @param[url] - The url for the image to be loaded
+ * @param[placeholderRes] - An optional drawable res to be used as image placeholder while it is loading
+ * @param[errorRes] - An optional drawable res to be used as image error placeholder if the image wasn't
+ *              loaded properly
+ */
 fun ImageView.loadImageFromUrl(url: String,
-                               @DrawableRes placeholder: Int = -1,
-                               @DrawableRes error: Int = -1) {
+                               @DrawableRes placeholderRes: Int = -1,
+                               @DrawableRes errorRes: Int = -1) {
     val rc :RequestCreator = Picasso.with(context).
             load(url)
             .transform(CircleTransform())
 
-    if(placeholder != -1) rc.placeholder(placeholder)
-    if(error != -1) rc.error(error)
+    if(placeholderRes != -1) rc.placeholder(placeholderRes)
+    if(errorRes != -1) rc.error(errorRes)
 
     rc.into(this)
 
