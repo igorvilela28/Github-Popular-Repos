748c69ae17534beae47e9fa1f32a7b0c628a46f2
Igor Vilela <igorvilela28@hotmail.com>
2017-10-16 10:42:00 -0200

    Add base presenter and view

---
+++ app/src/main/java/com/igorvd/githubrepo/presentation/BasePresenter.kt
@@ -0,0 +1,11 @@
+package com.igorvd.githubrepo.presentation
+
+/**
+ * @author Igor Vilela
+ * @since 14/10/17
+ */
+interface BasePresenter {
+
+    fun detachView()
+
+}
---
+++ app/src/main/java/com/igorvd/githubrepo/presentation/BaseView.kt
@@ -0,0 +1,12 @@
+package com.igorvd.githubrepo.presentation
+
+/**
+ * @author Igor Vilela
+ * @since 14/10/17
+ */
+interface BaseView {
+
+    fun showProgress();
+    fun hideProgress();
+    fun showError();
+}
